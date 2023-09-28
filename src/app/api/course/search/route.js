b4f3d88d6ad3bec631941b54aa2e674f89ccddac
Chayanin <arm.crazyplay@gmail.com>
2023-09-28 11:30:46 +0700

    first commit

---
+++ src/app/api/course/search/route.js
@@ -0,0 +1,12 @@
+import { getPrisma } from "@/libs/getPrisma";
+import { NextResponse } from "next/server";
+
+export const GET = async (request) => {
+  const searchText = request.nextUrl.searchParams.get("searchText");
+  const prisma = getPrisma();
+
+  //Modify following line so that it find course with "searchText" variable
+  const courses = await prisma.course.findMany({});
+
+  return NextResponse.json({ ok: true, courses });
+};
